c5de5768cfaa4fcd117d53b01d65ce49e0cbb0e7
Elena Kirilova <el_b_k@mail.bg>
2013-10-01 16:24:04 +0300

    main page

--- TellToAsk/TellToAsk/Controllers/HomeController.cs
+++ TellToAsk/TellToAsk/Controllers/HomeController.cs
@@ -10,6 +10,16 @@ namespace TellToAsk.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.QuestionsData = new dynamic[] 
+           {
+                new {category="Asia",value=53.8,color="#9de219"},
+                new {category="Europe",value=16.1,color="#90cc38"},
+                new {category="LatinAmerica",value=11.3,color="#068c35"},
+                new {category="Africa",value=9.6,color="#006634"},
+                new {category="MiddleEast",value=5.2,color="#004d38"},
+                new {category="NorthAmerica",value=3.6,color="#033939"}
+           };
+
             return View();
         }
 

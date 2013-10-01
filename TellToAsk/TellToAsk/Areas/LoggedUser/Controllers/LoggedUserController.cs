2151bb7859d900d2d1682e153236cae106f21ca2
Elena Kirilova <el_b_k@mail.bg>
2013-10-01 20:26:55 +0300

    add logged user area

---
+++ TellToAsk/TellToAsk/Areas/LoggedUser/Controllers/LoggedUserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TellToAsk.Areas.LoggedUser.Controllers
+{
+    public class LoggedUserController : Controller
+    {
+        //
+        // GET: /LoggedUser/LoggedUser/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
--- TellToAsk/TellToAsk/Areas/LoggedUser/LoggedUserAreaRegistration.cs
+++ TellToAsk/TellToAsk/Areas/LoggedUser/LoggedUserAreaRegistration.cs
@@ -16,7 +16,7 @@ namespace TellToAsk.Areas.LoggedUser
         {
             context.MapRoute(
                 "LoggedUser_default",
-                "LoggedUser/{controller}/{action}/{id}",
+                "{controller}/{action}/{id}",
                 new { action = "Index", id = UrlParameter.Optional }
             );
         }

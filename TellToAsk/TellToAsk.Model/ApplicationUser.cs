8f363ebc18a31a5c9eaf013c6a0f66569b038dc1
Vasil Lazarov <vaskosound@gmail.com>
2013-10-01 13:34:24 +0300

    Intial

---
+++ TellToAsk/TellToAsk.Model/ApplicationUser.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TellToAsk.Model
+{
+    public class ApplicationUser : User
+    {
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public int Points { get; set; }
+
+        public virtual ICollection<Question> MyQuestions { get; set; }
+
+        public virtual ICollection<Question> AnsweredQuestions { get; set; }
+
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        public virtual ICollection<Category> Categories { get; set; }
+
+        public ApplicationUser()
+        {
+            this.MyQuestions = new HashSet<Question>();
+            this.AnsweredQuestions = new HashSet<Question>();
+            this.Answers = new HashSet<Answer>();
+            this.Categories = new HashSet<Category>();
+        }
+
+    }
+}
---
+++ TellToAsk/TellToAsk.Model/Question.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TellToAsk.Model
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+
+        public string Text { get; set; }
+
+        public bool IsApprove { get; set; }
+
+        public virtual ApplicationUser Creator { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public Question()
+        {
+            this.Users = new HashSet<ApplicationUser>();
+            this.Answers = new HashSet<Answer>();
+        }
+    }
+}

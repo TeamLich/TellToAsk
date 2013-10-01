7e87e621c765b5ff9f190586e26de0f4675ec200
Dilyan Dichov <ddichov@gmail.com>
2013-10-01 14:03:11 +0300

    kendo included

--- TellToAsk/TellToAsk/App_Start/BundleConfig.cs
+++ TellToAsk/TellToAsk/App_Start/BundleConfig.cs
@@ -8,8 +8,16 @@ namespace TellToAsk
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // The Kendo JavaScript bundle
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/Kendo/jquery.min.js",
+                        "~/Scripts/Kendo/kendo.all.min.js", // or kendo.all.min.js if you want to use Kendo UI Web and Kendo UI DataViz
+                        "~/Scripts/Kendo/kendo.aspnetmvc.min.js"));
+
+            // The Kendo CSS bundle
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                        "~/Content/Kendo/kendo.common.min.css",
+                        "~/Content/Kendo/kendo.default.min.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +34,14 @@ namespace TellToAsk
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/bootstrap-responsive.css"));
+
+            // Clear all items from the ignore list to allow minified CSS and JavaScript files in debug mode
+            bundles.IgnoreList.Clear();
+
+            // Add back the default ignore list rules sans the ones which affect minified files and debug mode
+            bundles.IgnoreList.Ignore("*.intellisense.js");
+            bundles.IgnoreList.Ignore("*-vsdoc.js");
+            bundles.IgnoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
         }
     }
 }
